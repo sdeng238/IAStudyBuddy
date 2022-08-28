1606614337080345d379a4e394b4fff73a5135da
sdeng238 <shirleyd2023@student.cis.edu.hk>
2022-08-28 19:52:26 +0800

    Task 14 (cannot change RecView button colour)

--- app/src/main/java/com/example/iastudybuddy/FocusFragment.java
+++ app/src/main/java/com/example/iastudybuddy/FocusFragment.java
@@ -1,9 +1,14 @@
 package com.example.iastudybuddy;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.SystemClock;
 import android.view.LayoutInflater;
@@ -11,6 +16,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Chronometer;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -21,16 +33,22 @@ public class FocusFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore firestore;
+
     private Button goToAddSubjectActivityButton;
 
     private Chronometer chronometer;
     //time difference from when user starts chronometer from when they paused it
     private long pauseOffset;
     private boolean running;
-
     private Button chrStartButton;
     private Button chrPauseButton;
     private Button chrResetButton;
+
+    private RecyclerView subjectsRecView;
+    private ArrayList<Subject> subjectsList;
+    private int[] subjectColours; //NEW
 
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
@@ -76,6 +94,13 @@ public class FocusFragment extends Fragment {
         // Inflate the layout for this fragment
         final View v = inflater.inflate(R.layout.fragment_focus, container, false);
 
+        mAuth = FirebaseAuth.getInstance();
+        //DELETE ALL mUSERs
+        firestore = FirebaseFirestore.getInstance();
+
+        goToAddSubjectActivityButton = v.findViewById(R.id.goToAddSubjectButton);
+        goToAddSubjectActivityButton.setOnClickListener(view -> startActivity(new Intent(getActivity(), AddSubjectActivity.class)));
+
         //hour and colon are automatically added when one hour is reached
         chronometer = v.findViewById(R.id.chronometer);
         //set as Time: (chronometer) when activity starts
@@ -83,6 +108,7 @@ public class FocusFragment extends Fragment {
 //        chronometer.setBase(SystemClock.elapsedRealtime());
 
         chrStartButton = v.findViewById(R.id.chrStartButton);
+//        chrStartButton.setBackgroundColor(getResources().getColor(R.color.blue));
         chrStartButton.setOnClickListener(view ->
         {
             //if not running, be able to start chronometer
@@ -119,9 +145,115 @@ public class FocusFragment extends Fragment {
             pauseOffset = 0;
         });
 
-        goToAddSubjectActivityButton = v.findViewById(R.id.goToAddSubjectButton);
-        goToAddSubjectActivityButton.setOnClickListener(view -> startActivity(new Intent(getActivity(), AddSubjectActivity.class)));
+
+        //array of subject colour ints
+        subjectColours = new int[]{getResources().getColor(R.color.red), getResources().getColor(R.color.orange), getResources().getColor(R.color.yellow), getResources().getColor(R.color.green), getResources().getColor(R.color.blue), getResources().getColor(R.color.purple), getResources().getColor(R.color.pink)};
+        //ArrayList of user's Subject objects
+        subjectsList = new ArrayList<>();
+        //fetch user's subjects
+        firestore.collection("subjects").whereEqualTo("ownerEmail", mAuth.getCurrentUser().getEmail()).get().addOnCompleteListener(task ->
+        {
+            if(task.isSuccessful())
+            {
+                for(DocumentSnapshot ds : task.getResult().getDocuments())
+                {
+                    Subject currSubject = ds.toObject(Subject.class);
+
+                    subjectsList.add(currSubject);
+                }
+
+                subjectsRecView = v.findViewById(R.id.subjectsRecView);
+                SubjectsAdapter myAdapter = new SubjectsAdapter(subjectsList, subjectColours);
+                subjectsRecView.setAdapter(myAdapter);
+                subjectsRecView.setLayoutManager(new LinearLayoutManager(v.getContext()));
+            }
+        });
 
         return v;
+    }
+
+
+    public static class SubjectsAdapter extends RecyclerView.Adapter<SubjectsViewHolder>
+    {
+        ArrayList<Subject> subjectsData;
+        int[] subjectColours;
+
+        public SubjectsAdapter(ArrayList data, int[] colours)
+        {
+            subjectsData = data;
+            subjectColours = colours;
+        }
+
+        @NonNull
+        @Override
+        public SubjectsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
+        {
+            View myView = LayoutInflater.from(parent.getContext()).inflate(R.layout.subjects_row_view, parent, false);
+
+            SubjectsViewHolder holder = new SubjectsViewHolder(myView);
+
+            return holder;
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull SubjectsViewHolder holder, int position)
+        {
+            holder.subjectNameText.setText(subjectsData.get(position).getName());
+            //if colour of task's subject colour matches with current subject colour, set colour
+            if(subjectsData.get(position).getColour().equals("red"))
+            {
+                holder.subjectNameText.setTextColor(subjectColours[0]);
+            }
+            else if(subjectsData.get(position).getColour().equals("orange"))
+            {
+                holder.subjectNameText.setTextColor(subjectColours[1]);
+            }
+            else if(subjectsData.get(position).getColour().equals("yellow"))
+            {
+                holder.subjectNameText.setTextColor(subjectColours[2]);
+            }
+            else if(subjectsData.get(position).getColour().equals("green"))
+            {
+                holder.subjectNameText.setTextColor(subjectColours[3]);
+            }
+            else if(subjectsData.get(position).getColour().equals("blue"))
+            {
+                holder.subjectNameText.setTextColor(subjectColours[4]);
+            }
+            else if(subjectsData.get(position).getColour().equals("purple"))
+            {
+                holder.subjectNameText.setTextColor(subjectColours[5]);
+            }
+            else if(subjectsData.get(position).getColour().equals("pink"))
+            {
+                holder.subjectNameText.setTextColor(subjectColours[6]);
+            }
+
+            ArrayList<Integer> todayFocusTime = subjectsData.get(position).getTodayFocusTime();
+            holder.subjectTimeText.setText(todayFocusTime.get(0) + ":" + todayFocusTime.get(1) + ":" + todayFocusTime.get(2));
+        }
+
+        @Override
+        public int getItemCount()
+        {
+            return subjectsData.size();
+        }
+    }
+
+
+    public static class SubjectsViewHolder extends RecyclerView.ViewHolder
+    {
+        protected TextView subjectNameText;
+        protected TextView subjectTimeText;
+        protected Button startStopButton;
+
+        public SubjectsViewHolder(@NonNull View itemView)
+        {
+            super(itemView);
+
+            subjectNameText = itemView.findViewById(R.id.srSubjectNameTextView);
+            subjectTimeText = itemView.findViewById(R.id.srSubjectTimeTextView);
+            startStopButton = itemView.findViewById(R.id.startStopButton);
+        }
     }
 }
